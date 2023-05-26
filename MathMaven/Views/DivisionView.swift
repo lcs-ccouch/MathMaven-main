53d8ceb6674c73c6aecc345025502ce5de5897b1
Calum Couch <ccouch@lcs.on.ca>
2023-05-25 23:27:42 -0400

    I added the ability to round to the nearest whole number instead.

--- MathMaven/Views/DivisionView.swift
+++ MathMaven/Views/DivisionView.swift
@@ -1,142 +1,97 @@
-//
-//  DivisionView.swift
-//  MathMaven
-//
-//  Created by Russell Gordon on 2023-05-24.
-//
-
 import SwiftUI
 
 struct DivisionView: View {
     
+    @State private var firstValue = 0
+    @State private var secondValue = 0
+    @State private var userAnswer = ""
+    @State private var answerChecked = false
+    @State private var isAnswerCorrect = false
     
-    // MARK: Stored properties
-    
-    // The numbers to be subtracted
-    @State var firstValue = Int.random(in: 1...144)
-    @State var secondValue = Int.random(in: 1...72)
-    
-    // Holds whatever input was provided by the user
-    @State var input = ""
-    
-    // Has the user's answer been checked?
-    @State var answerChecked = false
-    
-    // Was the user's given answer correct?
-    @State var answerCorrect = false
-    
-    // MARK: Computed properties
-    
-    // The correct response
-    var correctResponse: Int {
-        return Int((firstValue/secondValue))
-       
+    var correctAnswer: Int {
+        return computeCorrectAnswer()
     }
     
+    private func computeCorrectAnswer() -> Int {
+        return firstValue / secondValue
+    }
     
-    // The user interface to show
     var body: some View {
-        
-        VStack(spacing: 0) {
+        VStack {
+            Text("Division")
+                .font(.largeTitle)
+                .padding(.bottom, 20)
             
-            // 1. Present question
-            Group {
-                HStack {
-                    Text(Operation.division.rawValue)
-                    
-                    Spacer()
-                    
-                    VStack(alignment: .trailing) {
-                        Text("\(firstValue)")
-                        Text("\(secondValue)")
-                    }
-                }
-                
-                Divider()
+            Text("\(firstValue) ÷ \(secondValue)")
+                .font(.title)
+                .padding(.bottom, 40)
+            
+            TextField("Enter your answer", text: $userAnswer)
+                .keyboardType(.numberPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.bottom, 40)
+            
+            Button(action: {
+                checkAnswer()
+            }) {
+                Text("Check Answer")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
             }
-            .padding(.horizontal)
+            .disabled(answerChecked)
             
-            // 2. Accept answer
-            HStack {
-                
-                ZStack {
-                    
-                    // Only show this when the answer was found to be correct
-                    if answerCorrect == true {
-                        Image(systemName: "checkmark.circle")
-                            .foregroundColor(.green)
-                    }
-                    
-                    // Show this when the answer was checked and found to be false
-                    if answerChecked == true && answerCorrect == false {
-                        Image(systemName: "x.square")
-                            .foregroundColor(.red)
-                    }
-                }
-                
-                Spacer()
-                
-                TextField("",
-                          text: $input)
-                .multilineTextAlignment(.trailing)
-            }
-            .padding(.horizontal)
-            
-            
-            
-            // 3. Check answer
-            //    Only show button when answer has not already been checked
-            if answerChecked == false {
-                
-                CheckAnswerButtonView(input: input,
-                                      correctResponse: correctResponse,
-                                      answerChecked: $answerChecked,
-                                      answerCorrect: $answerCorrect)
-                
-            } else {
-                
-                // 4. Generate new question
-                // Only show this once an answer has been provided
-                Button(action: {
-                    generateNewQuestion()
-                }, label: {
-                    Text("New question")
-                        .font(.largeTitle)
-                })
-                .padding()
-                .buttonStyle(.bordered)
-                
+            if answerChecked {
+                Text(isAnswerCorrect ? "Correct!" : "Incorrect!")
+                    .font(.title)
+                    .foregroundColor(isAnswerCorrect ? .green : .red)
+                    .padding(.top, 20)
             }
             
-            // Push interface up to top of screen
             Spacer()
             
+            Button(action: {
+                generateNewQuestion()
+            }) {
+                Text("New Question")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding(.bottom, 20)
+            .disabled(!answerChecked)
         }
-        .font(Font.custom("SF Pro", size: 64))
+        .padding()
+        .onAppear {
+            generateNewQuestion()
+        }
     }
-    // MARK: Functions
     
-    // Generate a new question
     func generateNewQuestion() {
-        
-        // Generate a new question
-        firstValue = Int.random(in: 1...72)
-        secondValue = Int.random(in: 1...72)
-
-        // Reset properties that track what's happening with the current question
+        firstValue = Int.random(in: 1...144)
+        secondValue = Int.random(in: 1..<firstValue)
+        userAnswer = ""
         answerChecked = false
-        answerCorrect = false
-        
-        // Reset the input field
-        input = ""
-
+        isAnswerCorrect = false
+    }
+    
+    func checkAnswer() {
+        if let answer = Int(userAnswer) {
+            isAnswerCorrect = answer == correctAnswer
+        } else {
+            isAnswerCorrect = false
+        }
+        answerChecked = true
     }
 }
-
-
 
 struct DivisionView_Previews: PreviewProvider {
     static var previews: some View {
         DivisionView()
     }
+    
 }
